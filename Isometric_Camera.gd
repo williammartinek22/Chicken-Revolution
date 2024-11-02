0771b1e16bfa17ae7a19117103b27f1770280fca
williammartinek22 <wdavemart@gmail.com>
2024-11-02 16:00:04 -0400

    Added isometric camera, with isometric movement toggle

---
+++ Isometric_Camera.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+const SCROLL_SPEED = 10
+var perspectiveMovement = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	perspectiveMovement = $Control/CheckBox.button_pressed
+	
+	if Input.is_key_pressed(KEY_W):
+		if (perspectiveMovement):
+			translate(Vector3(SCROLL_SPEED * delta,0,-SCROLL_SPEED * delta))
+		else:
+			translate(Vector3(SCROLL_SPEED * delta,0,0))
+	
+	if Input.is_key_pressed(KEY_S):
+		if (perspectiveMovement):
+			translate(Vector3(-SCROLL_SPEED * delta,0,SCROLL_SPEED * delta))
+		else:
+			translate(Vector3(-SCROLL_SPEED * delta,0,0))
+		
+	if Input.is_key_pressed(KEY_A):
+		if (perspectiveMovement):
+			translate(Vector3(-SCROLL_SPEED*delta,0,-SCROLL_SPEED*delta))
+		else:
+			translate(Vector3(0,0,-SCROLL_SPEED * delta))
+		
+	if Input.is_key_pressed(KEY_D):
+		if (perspectiveMovement):
+			translate(Vector3(SCROLL_SPEED*delta,0,SCROLL_SPEED*delta))
+		else:
+			translate(Vector3(0,0,SCROLL_SPEED * delta))
