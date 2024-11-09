57cce4ddd8de895e0dbd235204e701318ca1c6fb
williammartinek22 <wdavemart@gmail.com>
2024-11-08 23:20:36 -0500

    Began outlining level 2

--- egg_projectile.gd
+++ egg_projectile.gd
@@ -10,11 +10,16 @@ func _ready():
 func _on_body_entered(body):
 	if body.is_in_group("Enemy"):
 		body.queue_free()
-		visible = false
-		$CollisionShape3D.set_deferred("disabled", true)
-		$AudioStreamPlayer.play()
-		await $AudioStreamPlayer.finished
-		queue_free()
+		crack()
+	elif body.get_parent() is StaticBody3D:
+		crack()
 
 func _process(delta):
 	position += -basis.z * delta * SPEED
+
+func crack():
+	visible = false
+	$CollisionShape3D.set_deferred("disabled", true)
+	$AudioStreamPlayer.play()
+	await $AudioStreamPlayer.finished
+	queue_free()
