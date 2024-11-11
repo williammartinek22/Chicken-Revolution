09027cad363ce5f3ed36ce7aabac7eed486bd249
ChukyFredj <chukyfredj@gmail.com>
2024-11-11 18:15:23 +0100

    First version of Hub
    I know it's ugly but it work ^^

---
+++ HubLevel.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+var chickens_collected = 0
+
+func _ready():
+	$Enclosure.body_entered.connect(_on_enclosure_body_entered)
+
+func _on_enclosure_body_entered(body):
+	if body.is_in_group("chickens"):
+		body.queue_free()
+		chickens_collected += 1
+		update_chicken_count_display()
+		
+		if chickens_collected >= 10:
+			show_level_buttons()
+
+func update_chicken_count_display():
+	$ChickenCountLabel.text = "Poulets récoltés : " + str(chickens_collected) + "/10"
+
+func show_level_buttons():
+	$Level1Button.visible = true
+	$Level2Button.visible = true
+
+func _on_level_1_button_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		get_tree().change_scene_to_file("res://test.tscn")
+
+func _on_level_2_button_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		get_tree().change_scene_to_file("res://level_2.tscn")
--- MainMenu.gd
+++ MainMenu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	show()
 	
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://test.tscn")
+	get_tree().change_scene_to_file("res://HubLevel.tscn")
 
 func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://OptionsMenu.tscn")
